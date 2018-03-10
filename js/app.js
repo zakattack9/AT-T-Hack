5989ad57625d93746d457d6d75515af3035cd98e
Zak <zakattack26111@gmail.com>
2018-03-10 09:06:57 -1000

    fixed image upload

--- js/app.js
+++ js/app.js
@@ -7,8 +7,29 @@ function grabCol(colors) {
 	})
 }
 
-var context = new AudioContext()
-var o = context.createOscillator()
-o.type = "sine"
-o.connect(context.destination)
-o.end()
+function convertor() {
+	console.log($('#displayImg').attr('src'))
+}
+
+// let fileInput = document.getElementById("fileIntake");
+
+// fileInput.accept = "image/*";
+// fileInput.require = true;
+
+// fileInput.addEventListener("change", () => {
+//   let reader = new FileReader();
+//   let displayImg = document.getElementById("displayImg");
+
+//   reader.addEventListener("load", () => {
+//       displayImg.src = reader.result;
+//       console.log(displayImg.src)
+//   }, false)
+
+//   reader.readAsDataURL(fileInput.files[0])
+// })
+
+
+var loadFile = function(event) {
+  var output = document.getElementById('displayImg');
+  output.src = URL.createObjectURL(event.target.files[0]);
+};
