b4fa52460904317e21f55ddf03c7027263f61456
angela g <angelageroniimo@gmail.com>
2018-03-10 12:34:49 -1000

    aligned color boxes using flex

--- js/app.js
+++ js/app.js
@@ -6,3 +6,15 @@ function grabCol(colors) {
 		$('#container').append(`<div class="color" style="background-color:rgb(${currVal[0]}, ${currVal[1]}, ${currVal[2]})"></div>`);
 	})
 }
+
+
+//makes dynamic element the same height and width
+// var $elem = ​$('div');
+// $(window).resize(function () {
+//     $elem.height($elem.width());
+// }).resize();
+
+let $img = $('#playImg');
+$(window).resize(function () {
+	$img.height($img.width());
+}).resize();
